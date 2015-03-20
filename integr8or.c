67cc985269c00208a890b2f4f4814216a3d36290
John Tramm <john.tramm@gmail.com>
2015-03-20 18:50:23 -0400

    dev

--- integr8or.c
+++ integr8or.c
@@ -1,6 +1,23 @@
 #include<stdlib.h>
 
-
-double integr8or( void )
+double integr8or( double (*f)(double), double min, double max, int gp )
 {
+	double range = max - min;
+	double del = range / gp;
+	double integral = 0;
+
+	for( int i = 0; i < gp; i++ )
+	{
+		double low = min + del*i;
+		double high = min + del*(i+1);
+		double mid = (low+high)/2;
+
+		double A = (*f)(low);
+		double B = (*f)(mid);
+		double C = (*f)(high);
+
+		integral += (high-low)/6.0 * (A + 4.0*B + C);
+	}
+
+	return integral;
 }

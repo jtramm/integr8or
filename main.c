675b207c6f9fdd57e4c55517504f26216931cea1
John Tramm <john.tramm@gmail.com>
2015-03-21 16:57:17 -0400

    working basics

--- main.c
+++ main.c
@@ -3,11 +3,10 @@
 #include "integr8or_header.h"
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 
 #define Z 13
-#define A 27
 #define Ed 25.0
-#define density 2.70
 
 double v(double E);
 double Sn(double E);
@@ -17,35 +16,43 @@ double Se(double E);
 
 int main(void)
 {
-	double high = 200;
 	int gp = 1000;
 	int gp_plot = 10;
-	double delta = (high - 2.0 * Ed) / gp_plot;
-
-	double NA = 6.022e23;
-	double N = density * NA / 27.0;
+	
+	// Energy Grid
+	double * E = (double *) malloc( gp_plot * sizeof(double));
+	for( int i = 0; i < gp_plot; i++ )
+	{
+		double delta = 7.0 / gp_plot;
+		E[i] = pow(10.0, delta*i);
+	}
 
 	// Plot variable high value from Ed to high
 	for( int i = 0; i < gp_plot; i++ )
 	{
-		double E = 2.0*Ed + i*delta;
-		double integral = integr8or(v, 2.0*Ed, E, gp);
-		double val = 2.0 / E * (N * integral);
-		printf("%.2lf\t%e\n", E, val);
+		double integral = integr8or(v, 0.0001, E[i], gp);
+		double val = integral / (2.0*Ed);
+		printf("%.2lf\t%e\n", E[i], val);
 	}
-	
+	double integral = integr8or(v, 0.0001, 50, gp);
+	//double val = integral / (2.0*Ed);
+	printf("%e\n", integral);
+
 	return 0;
 }
 
 double v(double E)
 {
-	return Sn(E) + Se(E);
+	E *= 1e-3;
+	return Sn(E) / (Sn(E) + Se(E));
 }
 
 double Sn(double E)
 {
 	double numerator = 8.462e-15 * Z*Z * Sigma_n(E);
-	double denominator = 4 * pow(Z,0.23);
+	double denominator = 4.0 * pow(Z,0.23);
+	if( denominator <= 0 )
+		printf("SN denom = %lf\n", denominator);
 
 	return numerator/denominator;
 }
@@ -53,8 +60,13 @@ double Sn(double E)
 double Sigma_n(double E)
 {
 	double numerator = 0.5 * log( 1.0 + 1.1383 * Epsilon(E));
+	double epE = Epsilon(E);
+	if( epE <= 0 )
+		printf("epE = %lf\n", epE);
 	double denominator = Epsilon(E) + 0.01321*pow(Epsilon(E),0.21226)
-		+ 0.19593 * sqrt(Epsilon(E));
+		+ 0.19593 * pow(Epsilon(E),0.5);
+	if( denominator <= 0 )
+		printf("Sigma_n denom = %lf\n", denominator);
 
 	return numerator/denominator;
 }
@@ -63,7 +75,9 @@ double Epsilon(double E)
 {
 	double numerator = 32.53 * E;
 	double denominator = 4 * Z*Z * pow(Z,0.23);
-	
+	if( denominator <= 0 )
+		printf("Epsilon denom = %lf\n", denominator);
+
 	return numerator/denominator;
 }
 
